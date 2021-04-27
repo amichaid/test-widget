6a2cb2e0ce937afa9fbd9971fc2d6dca92642197
Amichai Dani <amichaid@elementor.com>
2021-04-27 07:52:05 -0700

    First commit

---
+++ class-widget-test.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * Class Elementor_Test_Widget
+ */
+class Elementor_Test_Widget extends \Elementor\Widget_Base
+{
+
+    public function get_name()
+    {
+        return 'test_widget';
+    }
+
+    public function get_title()
+    {
+        return __('Test Widget', 'test_widget');
+    }
+
+    public function get_icon()
+    {
+        return 'eicon-custom-css';
+    }
+
+    public function get_categories()
+    {
+        return ['basic'];
+    }
+
+    protected function _register_controls()
+    {
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => __('Content', 'test_widget'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'title',
+            [
+                'label' => __('Card Title', 'test_widget'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'input_type' => 'text',
+                'placeholder' => __('Title', 'test_widget'),
+            ]
+        );
+
+        $this->add_control(
+            'description',
+            [
+                'label' => __('Card Description', 'test_widget'),
+                'type' => \Elementor\Controls_Manager::TEXTAREA,
+                'rows' => 10,
+                'placeholder' => __('Description', 'test_widget'),
+            ]
+        );
+
+        $this->add_control(
+            'image',
+            [
+                'label' => __('Card Image', 'test_widget'),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'default' => [
+                    'url' => \Elementor\Utils::get_placeholder_image_src(),
+                ]
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
+    protected function render()
+    {
+
+        // Get settings.
+        $img_settings = $this->get_settings_for_display('image');
+        $title = $this->get_settings_for_display('title');
+        $description = $this->get_settings_for_display('description');
+
+        if (!isset($img_settings) || !is_array($img_settings) || empty($img_settings)) {
+            return;
+        }
+
+        // Start card.
+        echo '<div class="test-widget">';
+
+        // Card image.
+        $img_id = $img_settings['id'];
+
+        if (isset($img_id) && is_int($img_id) && 0 < $img_id) {
+
+            $img_array = wp_get_attachment_image_src($img_id, 'thumbnail', false);
+            $img_url = $img_array[0];
+
+            if (isset($img_url) && !empty($img_url)) {
+
+                echo '<div class="test-widget-img-container">';
+                echo '<img src="' . esc_url($img_url) . '"/>';
+                echo '</div>';
+
+            }
+        }
+
+        // Card content.
+        echo '<div class="test-widget-content">';
+
+        // Card title.
+        if (isset($title) && is_string($title) && !empty($title)) {
+            echo '<div class="test-widget-title">' . esc_html($title) . '</div>';
+        }
+
+        // Card description.
+        if (isset($description) && is_string($description) && !empty($description)) {
+            echo '<div class="test-widget-description">' . esc_html($description) . '</div>';
+        }
+
+        echo '</div>'; // Card content end.
+        echo '</div>'; // Card end.
+    }
+}
---
+++ widget-test.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Widget Test
+ * Version: 1.0
+ */
+
+define('TEST_WIDGET_URL',plugin_dir_url(__FILE__));
+const TEST_WIDGET_VER = 1.0;
+
+add_action('init', 'init_widget_test');
+add_action('wp_enqueue_scripts', 'widget_test_enqueue_styles');
+
+function init_widget_test()
+{
+    require 'class-widget-test.php';
+
+    $test_widget = new Elementor_Test_Widget();
+
+    // Let Elementor know about our widget
+    \Elementor\Plugin::instance()->widgets_manager->register_widget_type($test_widget);
+}
+
+function widget_test_enqueue_styles()
+{
+    wp_enqueue_style('test_widget_style', TEST_WIDGET_URL . '/assets/test-widget.css', array(), true);
+}
